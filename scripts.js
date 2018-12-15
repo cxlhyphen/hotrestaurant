862061c339e0245ae42360abd47eefcb9762a25b
cindy <42073246+cxlhyphen@users.noreply.github.com>
2018-12-15 16:09:48 -0500

    start on click for submit button

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
 //FE Scripts
 
 //Grab form data
+//on button submit...
+$("#submit").on("click", function() {
+
+    let newRes = {
+        name: $("#name").val().trim(),
+        number: $("#phone").val().trim(),
+        email: $("#email").val().trim(),
+        guests: $("#guests").val().trim()
+    };
+
+    console.log(newRes);
+
+});
 
 //POST form data to server
