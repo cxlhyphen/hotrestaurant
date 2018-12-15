1527a3e45fa27ac2a67891c6b4c5a65322264a0b
cindy <42073246+cxlhyphen@users.noreply.github.com>
2018-12-15 16:44:08 -0500

    started post scripts

--- scripts.js
+++ scripts.js
@@ -18,3 +18,19 @@ $("#submit").on("click", function() {
 });
 
 //POST form data to server
+let URL = window.location.origin;
+
+//$.post(url,[data],[callback],[type]);
+/*
+Parameter Description:
+url: request url from which you want to submit & retrieve the data.
+data: json data to be sent to the server with request as a form data.
+callback: function to be executed when request succeeds.
+type: data type of the response content.
+*/
+
+$.post(URL+"/api/tables", newRes, function(newRes) {
+    //
+
+    //
+});
